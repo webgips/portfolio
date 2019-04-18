037e600c29e2263cd23fd4d27345f75ae955cbf6
Maxim Markov <markovmaximm@gmail.com>
2019-04-18 20:12:01 +0300

    update workItem

--- src/components/WorkItem/WorkItem.js
+++ src/components/WorkItem/WorkItem.js
@@ -4,10 +4,18 @@ import {works} from '../../assets/data.json'
 import "./WorkItem.scss";
 
 function WorkItem({match}){
+    const workInfo = works.filter((work) => work.name === match.params.id )[0]
     return (
         <div className="content works">
-          <div className="content__title">{match.params.id}</div>
-            {console.log(match)}
+          <div className="content__title">{workInfo.name}</div>
+            <div className="work-item">
+              <div className="work-item__text">
+                {workInfo.about}
+              </div>
+              <a href={workInfo.link} target="_blank" rel="noopener noreferrer" className="work-item__link">Link to website</a> 
+              <img className="work-item__img" src={workInfo.imgUrl} alt=""/>
+              
+            </div>
         </div>
     );
   }
