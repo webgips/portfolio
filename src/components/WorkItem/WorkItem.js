869b0538f7683807035af7ec00decf07f6771f62
Maxim Markov <markovmaximm@gmail.com>
2019-04-19 18:00:54 +0300

    update workitem component

--- src/components/WorkItem/WorkItem.js
+++ src/components/WorkItem/WorkItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from "react-router-dom"
 import {works} from '../../assets/data.json'
 import "./WorkItem.scss";
 
@@ -9,8 +9,25 @@ function WorkItem({match}){
         <div className="content works">
           <div className="content__title">{workInfo.name}</div>
             <div className="work-item">
-              <div className="work-item__text">
+              <div className="work-item__desc">
                 {workInfo.about}
+              </div>
+              <div>
+                <div className="work-item__tech">
+                  used technologies stack
+                </div>
+                <ul className="work-item__tech-list">
+                  {
+                    workInfo.stack.map((tech,index) => 
+                      <li className="work-item__tech-item" key={index}>
+                        <Link to={{
+                          pathname: "/works",
+                          search: `?sort=${tech}`,
+                        }} className="tech-link">{tech}</Link>
+                      </li>
+                    )
+                  }
+                </ul>
               </div>
               <a href={workInfo.link} target="_blank" rel="noopener noreferrer" className="work-item__link">Link to website</a> 
               <img className="work-item__img" src={workInfo.imgUrl} alt=""/>
