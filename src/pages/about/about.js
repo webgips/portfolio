9b4c4027690b5a2ff3deb0cfbbc145875ceb807d
Maxim Markov <markovmaximm@gmail.com>
2019-04-23 02:57:13 +0300

    update about page

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,34 +1,38 @@
-import React  from 'react';
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
-import { stack } from '../../assets/data.json'
-import './about.scss'
+
+import "./about.scss";
 
 const About = () => {
-    return (
-        <div className="content about">
-            <h2 className="content__title">About</h2>
-            <div className="about__content">
-            <div className="about__name">Maxim Markov</div>
-            <div className="about__job">Frontend Developer</div>
-            <div className="about__desc">
-                I'm a 27-year-old developer from Minsk. I creates websites that are beautiful, interactive, responsive and friendly.</div>
-            <div className="about__stack">
-                <div className="about__stack-title">My stack:</div>
-                <div className="about__stack-list">
-                    {stack.map((tech, index) => <Link to={{
-                    pathname: "/works",
-                    search: `?sort=${tech}`,
-                    }} className="tech-link" key={index}>{tech}</Link>)}
-                </div>
-            </div>
-            <div className="about__btns">
-                <Link className="about__btns-hire about__btn" to="/contacts" exact="exact">Contact me</Link>
-            </div>
-            </div>
+  return (
+    <div className="content about">
+      <h2 className="content__title">About</h2>
+      <div className="about__content">
+        <div className="about__name">Maxim Markov</div>
+        <div className="about__job">Frontend Developer</div>
+        <div className="about__desc">
+          I'm a 27-year-old developer from Minsk. I creates websites that are
+          beautiful, interactive, responsive and friendly.
         </div>
-    )
-}
-    
+        <div className="about__stack">
+          <div className="about__stack-title">My stack:</div>
+          <div className="about__stack-list">
+            HTML5(Pug), CSS3(SCSS), JS(ES6), Vue.js(Vuex,Vue-router), React.js(React-router)
+          </div>
+        </div>
+        <div className="about__btns">
+          <Link
+            className="about__btns-hire about__btn"
+            to="/contacts"
+            exact="exact"
+          >
+            Contact me
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
