4dadc449fcb2a21da87d1394f65b58c93f0a36ac
Maxim Markov <markovmaximm@gmail.com>
2019-05-14 17:48:30 +0300

    fix

--- src/pages/contacts/contacts.js
+++ src/pages/contacts/contacts.js
@@ -1,17 +1,13 @@
 import React, { Component } from "react";
 import "./contacts.scss";
-import firebase from 'firebase/app';
+import firebase from "firebase/app";
 import "firebase/database";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCFHTIkgFfmZLscAgQuwqfOIi3lvvy65n8",
-    authDomain: "maxim-markov.firebaseapp.com",
-    databaseURL: "https://maxim-markov.firebaseio.com/"
-  };
-   
-firebase.initializeApp(firebaseConfig)
+import firebaseConfig from "./../../assets/.firebaseconfig"
+
+firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
-const messagesRef = database.ref('messages')
+const messagesRef = database.ref("messages");
 
 class Contacts extends Component {
   constructor(props) {
@@ -22,15 +18,14 @@ class Contacts extends Component {
       description: null,
       email: null,
       company: null,
-      messageSend:  sessionStorage.getItem("contactMessageSend") ? sessionStorage.getItem("contactMessageSend") : false
+      messageSend: sessionStorage.getItem("contactMessageSend")
+        ? sessionStorage.getItem("contactMessageSend")
+        : false
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-   /* hideForm(){
-    document.getElementById("contact__form").style.display = 'none'
-  } */
   handleInputChange(event) {
     const target = event.target;
     const value = target.type === "checkbox" ? target.checked : target.value;
@@ -42,81 +37,84 @@ class Contacts extends Component {
   }
   handleSubmit(event) {
     event.preventDefault();
-    messagesRef.push(this.state).then(()=>{
-      this.setState({
-        name: null,
-        description: null,
-        email: null,
-        company: null,
+    messagesRef
+      .push(this.state)
+      .then(() => {
+        this.setState({
+          name: null,
+          description: null,
+          email: null,
+          company: null
+        });
       })
-    })
-    .then(()=>{
-      sessionStorage.setItem("contactMessageSend","true");
-      this.setState({
-        messageSend: sessionStorage.getItem("contactMessageSend")
-      })
-    })
+      .then(() => {
+        sessionStorage.setItem("contactMessageSend", "true");
+        this.setState({
+          messageSend: sessionStorage.getItem("contactMessageSend")
+        });
+      });
   }
   render() {
     return (
       <div className="contact content">
         <h2 className="content__title">Contacts</h2>
         <div className="contact__content">
-          {this.state.messageSend && 
+          {this.state.messageSend && (
             <div className="contact__message">Your message has been sent.</div>
-          }
-          {!this.state.messageSend && <form
-            className="contact__form"
-            id="contact__form"
-            onSubmit={this.handleSubmit}
-          >
-            <div className="contact__text" />
-            <div className="contact__form-row">
-              <textarea
-                className="contact__form-textarea"
-                name="description"
-                minLength="10"
-                required="required"
-                placeholder="Project Type, Target customer, timeline, estimate etc"
-                onChange={this.handleInputChange}
-              />
-            </div>
-            <div className="contact__text">About You </div>
-            <div className="contact__form-about-wrap">
+          )}
+          {!this.state.messageSend && (
+            <form
+              className="contact__form"
+              id="contact__form"
+              onSubmit={this.handleSubmit}
+            >
+              <div className="contact__text" />
               <div className="contact__form-row">
-                <input
-                  className="contact__form-input"
-                  type="text"
-                  name="name"
-                  minLength="3"
+                <textarea
+                  className="contact__form-textarea"
+                  name="description"
+                  minLength="10"
                   required="required"
-                  placeholder="Full Name"
-                  onChange={this.handleInputChange}
-                />
-                <input
-                  className="contact__form-input"
-                  type="email"
-                  name="email"
-                  required="required"
-                  placeholder="Email Address"
+                  placeholder="Project Type, Target customer, timeline, estimate etc"
                   onChange={this.handleInputChange}
                 />
               </div>
-              <div className="contact__form-row">
-                <input
-                  className="contact__form-input"
-                  type="text"
-                  name="company"
-                  placeholder="Company Name"
-                  onChange={this.handleInputChange}
-                />
+              <div className="contact__text">About You </div>
+              <div className="contact__form-about-wrap">
+                <div className="contact__form-row">
+                  <input
+                    className="contact__form-input"
+                    type="text"
+                    name="name"
+                    minLength="3"
+                    required="required"
+                    placeholder="Full Name"
+                    onChange={this.handleInputChange}
+                  />
+                  <input
+                    className="contact__form-input"
+                    type="email"
+                    name="email"
+                    required="required"
+                    placeholder="Email Address"
+                    onChange={this.handleInputChange}
+                  />
+                </div>
+                <div className="contact__form-row">
+                  <input
+                    className="contact__form-input"
+                    type="text"
+                    name="company"
+                    placeholder="Company Name"
+                    onChange={this.handleInputChange}
+                  />
+                </div>
               </div>
-            </div>
-            <button className="contact__form-submit btn">
-              Request a Quote
-            </button>
-          </form>
-          }
+              <button className="contact__form-submit btn">
+                Request a Quote
+              </button>
+            </form>
+          )}
         </div>
       </div>
     );
